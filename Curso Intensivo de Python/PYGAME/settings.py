5e4945e5892ddeb64476f4be47c691b779f27ce9
Denylson Panzo <130487827+deias02@users.noreply.github.com>
2024-11-24 09:02:02 +0000

    Add files via upload

---
+++ Curso Intensivo de Python/PYGAME/settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """Uma classe para armazenar todas as configurações da Invasão Alienígena."""
+    def __init__(self):
+        """Inicializa as configurações do jogo."""
+        # Configurações da tela
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (135, 206, 250)  # Azul claro (Sky Blue)
